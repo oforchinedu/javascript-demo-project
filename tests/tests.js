68f4c3fbf3e6ec76492a5eaf76259092409bb45f
chinedu ofor <oforchinedu12@gmail.com>
2017-01-06 16:30:43 +0100

    using chai expect

--- tests/tests.js
+++ tests/tests.js
@@ -3,34 +3,37 @@
 'use strict'
 
 var chai = require('chai');
+var expect = chai.expect;
 var assert = chai.assert;
 
 var myApp = require('../app/library.js');
 
 describe("Test for proper arithmetic functionality", function() {
   it("should return 2 as average for 1, 2, 3", function() {
-    assert(myApp.computeAverage(1,2,3) == 3);
+    expect(myApp.computeAverage(1,2,3)).to.equal(2);
   })
   it("should return 5 as average for 3, 7, 5", function() {
-    assert(myApp.computeAverage(3, 7, 5) == 5);
+    expect(myApp.computeAverage(3,7,5)).to.equal(5);
   })
   it("should return 120 as factorial for 5", function() {
-    assert(myApp.computeFactorial(5) == 120);
+    expect(myApp.computeFactorial(5)).to.equal(120);
   })
 })
 
 
 describe("Test for temperature conversion functionality", function() {
   it("should return X for Celcius value Y", function() {
-    assert(myApp.convertTempCtoF(40) == 104);
+    expect(myApp.convertTempCtoF(40)).to.equal(104);
   })
   it("should return X for Celcius value Y", function() {
-    assert(myApp.convertTempCtoF(60) == 140);
+    expect(myApp.convertTempCtoF(60)).to.equal(140);
   })
   it("should return Y for Fahrenheit value X", function() {
-    assert(myApp.convertTempFtoC(140) == 60);
+    expect(myApp.convertTempFtoC(140)).to.equal(60);
   })
   it("should return Y for Fahrenheit value X", function() {
-    assert(myApp.convertTempFtoC(104) == 40);
+    expect(myApp.convertTempFtoC(104)).to.equal(40);
   })
 })
+
+// check documentations and use expect
